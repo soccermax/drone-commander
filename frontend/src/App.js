7ab5d60eeb82e79fa0067f7a1ae18d953449ced4
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2021-03-13 17:02:32 +0100

    initial project setup + handtrack.js + tensorflow.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
-// import "./App.css";
+import "./App.css";
 import React, {useRef, useState, useEffect} from "react";
-import Webcam from "react-webcam";
+import Webcam from "react-webcam"
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+
 
 import * as handTrack from 'handtrackjs';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
+
+
 function App() {
+  const classes = useStyles();
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   let isVideo = false;
@@ -126,6 +147,17 @@ function App() {
           }}
         />
       </header>
+<Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
